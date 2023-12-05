664ef786e546176419801f24db895acf98535e02
Andreas Kurz <off.andreaskurz+github@gmail.com>
2023-12-05 14:24:50 +0100

    added glsl file extension

--- lua/kurz/autocmd.lua
+++ lua/kurz/autocmd.lua
@@ -4,3 +4,9 @@ vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
         vim.cmd("setfiletype xml")
     end
 })
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+    pattern = { "*.vert", "*.frag" },
+    callback = function ()
+        vim.cmd("setfiletype glsl")
+    end
+})
