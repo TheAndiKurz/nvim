0b3221fe5f3be5342722fe56dc0e8502b6e1502e
Andreas Kurz <off.andreaskurz+github@gmail.com>
2024-02-01 22:42:45 +0100

    added remap to close a buffer with ctrl d

--- lua/kurz/remap.lua
+++ lua/kurz/remap.lua
@@ -6,3 +6,6 @@ vim.keymap.set("n", "<leader>tt", ":vs +terminal<CR>")
 
 -- leave terminal mode with <Esc>
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
+-- close the current buffer with <C-d>
+vim.keymap.set("n", "<C-d>", ":bd<CR>")
