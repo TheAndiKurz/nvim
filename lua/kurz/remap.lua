e442fcb3fc6a6b6a83cace2364bf858d33607785
Andreas Kurz <off.andreaskurz+github@gmail.com>
2024-08-13 10:20:51 +0200

    changed keybindings

--- lua/kurz/remap.lua
+++ lua/kurz/remap.lua
@@ -1,5 +1,9 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- open explorer with <leader>e
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>he", ":Sex<CR>")
+vim.keymap.set("n", "<leader>ve", ":Vex<CR>")
 
 -- open termianl in a split window with <leader>t
 vim.keymap.set("n", "<leader>th", ":sp +terminal<CR>")
@@ -10,3 +14,7 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 -- close the current buffer with <C-d>
 vim.keymap.set("n", "<C-d>", ":bd<CR>")
+
+-- open the command history with <leader>.
+vim.keymap.set("n", "<leader>.", "q:")
+
