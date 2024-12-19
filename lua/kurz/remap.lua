b8996eee3ebab50d2a681773eaab70d7ecf40ac7
Andreas Kurz <off.andreaskurz+github@gmail.com>
2024-12-19 20:09:46 +0100

    added floating terminal

--- lua/kurz/remap.lua
+++ lua/kurz/remap.lua
@@ -5,13 +5,6 @@ vim.keymap.set("n", "<leader>d", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>hd", ":Sex<CR>")
 vim.keymap.set("n", "<leader>vd", ":Vex<CR>")
 
--- open termianl in a split window with <leader>t
-vim.keymap.set("n", "<leader>ht", ":sp +terminal<CR>")
-vim.keymap.set("n", "<leader>vt", ":vs +terminal<CR>")
-
--- leave terminal mode with <Esc>
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
-
 -- close the current buffer with <C-d>
 vim.keymap.set("n", "<C-d>", ":bd<CR>")
 
