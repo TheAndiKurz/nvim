d041d02d51cd5f28bfa7240340cb8de7a18c65c6
Andreas Kurz <off.andreaskurz+github@gmail.com>
2024-12-20 09:53:52 +0100

    added keybind to open telescope with all currently open buffers

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,6 +2,9 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<leader>g', builtin.git_files, {})
+vim.keymap.set('n', '<leader>b', function ()
+    builtin.buffers({ sort_lastused = true, sort_mru = true })
+end)
 
 vim.keymap.set('n', '<leader>/', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
